1f843ffa3a66b959e544bb1f3214eb455e698265
pepaporrasmedina <pedroporrasmedina@gmail.com>
2022-03-29 13:43:54 -0600

    Adding gitignore file and configure Redis conection

--- inventory/main.py
+++ inventory/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from redis_om import get_redis_connection, HashModel
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+redis = get_redis_connection(
+    host="redis-13289.c52.us-east-1-4.ec2.cloud.redislabs.com",
+    port=13289,
+    password="nvzAKvSgwYYNbv17slBby8MMIH0JOi6q",
+    decode_responses=True
+)
+
+class Product(HashModel):
+    name: str
+    price: float
+    quantity_available: int
+
+    class Meta:
+        database = redis
+
+@app.get('/products')
+def all():
+    return Product.all_pks()
+
+
