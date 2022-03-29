f4ea1aa4b3848d5c9af38f14f6c7cb41fda840a1
pepaporrasmedina <pedroporrasmedina@gmail.com>
2022-03-29 14:10:43 -0600

    Creation of CRUD methods

--- inventory/main.py
+++ inventory/main.py
@@ -1,3 +1,4 @@
+from itertools import product
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
@@ -28,6 +29,27 @@ class Product(HashModel):
 
 @app.get('/products')
 def all():
-    return Product.all_pks()
+    return [format(pk) for pk in Product.all_pks()]
+
+def format(pk: str):
+    product = Product.get(pk)
+
+    return{
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity_available': product.quantity_available
+    }
+
+@app.post('/products')
+def create(product: Product):
+    return product.save()
 
 
+@app.get('/products/{pk}')
+def get(pk:str):
+    return Product.get(pk)
+
+@app.delete('/products/{pk}')
+def delete(pk: str):
+    return Product.delete(pk)
