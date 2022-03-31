7a4bc0bab7d72e69f6d8efc69f10bbd7b1dfcf02
pepaporrasmedina <pedroporrasmedina@gmail.com>
2022-03-31 01:23:51 -0600

    Adding Background tasks from FastAPI in order to manage the task executed after to push an order into redis

--- payment/main.py
+++ payment/main.py
@@ -3,10 +3,11 @@ from itertools import product
 from starlette.requests import Request
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from redis_om import get_redis_connection, HashModel
+from fastapi.background import BackgroundTasks  #using for asyncronuos task in order to avoid
 from decouple import config
+from redis_om import get_redis_connection, HashModel
 from unipath import Path
-import requests
+import requests, time
 
 
 BASE_DIR = Path(__file__).parent
@@ -44,36 +45,47 @@ class Order(HashModel):
     class Meta:
         database: redis
 
+
+@app.get('/orders/{pk}')
+def get(pk: str):
+    return Order.get(pk)
+
 @app.post('/orders')
-async def create(request: Request): #id, quantity
+async def create(request: Request, background_tasks: BackgroundTasks): #id, quantity
+
     body = await request.json()
 
-    req = requests.get('http://localhost:8000/products/%s' % body['id'])
+    req = requests.get('http://127.0.0.1:8000/products/%s' % body['id'])
+
     product = req.json()
 
     order = Order(
         product_id=body['id'],
         price=product['price'],
         fee=0.2*product['price'],
-        total= 1.2*product['price'],
-        quantity=body['id'],
+        total=1.2*product['price'],
+        quantity=body['quantity'],
         status='pending'
     )
-
     order.save()
 
-    order_completed(order)
+    background_tasks.add_task(order_completed, order)
 
     return order
 
+     #Just for test the data retrived form the inventory microservice
+    #return req.json()
+
 #Function to change the Order status by default
 def order_completed(order: Order):
+    time.sleep(5)
     order.status = 'completed'
     order.save()
 
 
-    #Just for test the data retrived form the inventory microservice
-    #return req.json()
+## if a Error :redis.exceptions.ConnectionError: Error 61 connecting to localhost:6379. Connection refused. happens porceed to install redis and run redis-server
+
+   
 
 
 
